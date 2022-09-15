9a5b7e96cba429bfc4777ec98f2c4497bba83867
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-15 09:42:20 +0300

    Person and IPerson and Student classes

---
+++ IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    internal interface IPerson
+    {
+        public void SayName(string value);
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    internal abstract class Person
+    {
+        public string Fname { get; set; }
+        public string  Lname { get; set; }
+    }
+}
