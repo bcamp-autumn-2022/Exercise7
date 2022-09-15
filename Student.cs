3991b504e75e3de986d52e5b7711689badcd9796
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-15 10:25:21 +0300

    Constructor works

--- Student.cs
+++ Student.cs
@@ -8,6 +8,13 @@ namespace Exercise7
 {
     internal class Student : Person, IPerson
     {
+        public Student() { }
+        public Student(string fn, string ln)
+        {
+            this.Fname = fn;
+            this.Lname = ln;
+        }
+
         public void SayName(string value)
         {
             Console.WriteLine(value);
